2991503018f8f107ab1c7eddad077dd38be6ccf0
HirokiOyabu <yab_82_hiro@icloud.com>
2020-01-29 03:59:21 +0900

    Finished!!

--- ylp.js
+++ ylp.js
@@ -1,32 +1,77 @@
-// let endpoint = new Date('2020-01-31T23:59:59');
-// let viewarea = document.getElementById('viewarea');
-
 function zeroPadding(num,length){
     return ('0000000000' + num).slice(-length);
 }
 
-document.addEventListener('touchmove', function(e) {e.preventDefault();}, {passive: false});
-
-const view = function() {
-    let endpoint = new Date('2020-01-31T16:45:00');
-    // let endpoint = new Date("Fri, Jan 31, 2020 07:45:00 +0900");
+function printTimer(diff) {
+    // 要素取得
     let viewarea = document.getElementById('lifepoint');
     let subviewarea = document.getElementById('sublifepoint');
-    // let nowu = new Date();
-    // let now = new Date(nowu.toUTCString().slice(0,-3) + '+0900');
-    // viewarea.innerHTML = endpoint.toString();
-    let now = new Date();
-    let diff = endpoint.getTime() - now.getTime();
-    // viewarea.innerHTML = parseInt((diff/1000));
+    // 残り時間表示
     viewarea.innerHTML = (diff/1000).toFixed(2);
-
+    // 単位変換
     let sec = parseInt(diff/1000);
     let d = sec/86400 | 0;
     let h = sec%86400/3600 | 0;
     let m = sec%86400%3600/60 | 0;
     let s = sec%60;
-
+    // 残り時間表示
     subviewarea.innerHTML = zeroPadding(d,2) + 'd ' + zeroPadding(h,2) + 'h ' + zeroPadding(m,2) + 'm ' + zeroPadding(s,2) + 's'
+}
+
+function printWait(diff) {
+    // 要素取得
+    let intitle = document.getElementById('intitle');
+    let viewarea = document.getElementById('lifepoint');
+    let subviewarea = document.getElementById('sublifepoint');
+    intitle.innerHTML = 'YOU CAN DO IT !!';
+    // 単位変換
+    let sec = parseInt(diff/1000);
+    let h = sec%86400/3600 | 0;
+    let m = sec%86400%3600/60 | 0;
+    let s = sec%60;
+    viewarea.innerHTML = zeroPadding(h,2) + 'h ' + zeroPadding(m,2) + 'm ' + zeroPadding(s,2) + 's'
+    subviewarea.innerHTML = 'Now is publishing\nthe graduation thesis';
+}
+
+function printAfter(diff) {
+    // 要素取得
+    let intitle = document.getElementById('intitle');
+    let viewarea = document.getElementById('lifepoint');
+    let subviewarea = document.getElementById('sublifepoint');
+    intitle.innerHTML = 'Since that day...';
+    // 単位変換
+    let sec = parseInt(diff/1000);
+    let d = sec/86400 | 0;
+    let h = sec%86400/3600 | 0;
+    let m = sec%86400%3600/60 | 0;
+    let s = sec%60;
+    // 経過時間表示
+    viewarea.innerHTML = d + 'd'
+    subviewarea.innerHTML = '+ ' + zeroPadding(h,2) + 'h ' + zeroPadding(m,2) + 'm ' + zeroPadding(s,2) + 's'
+}
+
+document.addEventListener('touchmove', function(e) {e.preventDefault();}, {passive: false});
+
+const view = function() {
+    // 現在標準時時間
+    let now = ((new Date()).getTime()) + (new Date()).getTimezoneOffset()*60*1000;
+
+    // 第一目標
+    let endpoint1 = (new Date(2020, 0, 31, 16, 45, 0, 0)).getTime() + (new Date()).getTimezoneOffset()*60*1000;
+    let endpoint2 = (new Date(2020, 1, 13, 14, 50, 0, 0)).getTime() + (new Date()).getTimezoneOffset()*60*1000;
+    let endpoint3 = (new Date(2020, 1, 13, 16, 30, 0, 0)).getTime() + (new Date()).getTimezoneOffset()*60*1000;
+
+    if (now < endpoint1) {
+        printTimer(endpoint1 - now);
+    }
+    else if (now < endpoint2) {
+        printTimer(endpoint2 - now);
+    }
+    else if (now < endpoint3) {
+        printWait(endpoint3 - now);
+    } else {
+        printAfter(now - endpoint3);
+    }
 }
 
 setInterval(view, 73);
