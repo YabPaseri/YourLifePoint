783d80f55c4d784402729b2008d531089d1d00f6
HirokiOyabu <yab_82_hiro@icloud.com>
2020-01-28 18:48:11 +0900

    initial commit

---
+++ ylp.js
@@ -0,0 +1,12 @@
+// let endpoint = new Date('2020-01-31T23:59:59');
+// let viewarea = document.getElementById('viewarea');
+
+const view = function() {
+    let endpoint = new Date('2020-01-31T23:59:59');
+    let viewarea = document.getElementById('lifepoint');
+    let now = new Date();
+    let diff = endpoint.getTime() - now.getTime();
+    viewarea.innerHTML = parseInt((diff/1000));
+}
+
+setInterval(view, 500);
