2ba22b99a8d91f9cafb493b79d13539267792965
HirokiOyabu <yab_82_hiro@icloud.com>
2020-01-28 18:57:07 +0900

    v2

--- ylp.js
+++ ylp.js
@@ -6,7 +6,8 @@ const view = function() {
     let viewarea = document.getElementById('lifepoint');
     let now = new Date();
     let diff = endpoint.getTime() - now.getTime();
-    viewarea.innerHTML = parseInt((diff/1000));
+    // viewarea.innerHTML = parseInt((diff/1000));
+    viewarea.innerHTML = (diff/1000).toFixed(3);
 }
 
-setInterval(view, 500);
+setInterval(view, 1);
