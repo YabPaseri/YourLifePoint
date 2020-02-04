2e2ad65f2552a669cbaacaf3d204ab93251c7865
HirokiOyabu <yab_82_hiro@icloud.com>
2020-02-04 16:41:51 +0900

    修正

--- ylp.js
+++ ylp.js
@@ -18,19 +18,19 @@ function printTimer(diff) {
     subviewarea.innerHTML = zeroPadding(d,2) + 'd ' + zeroPadding(h,2) + 'h ' + zeroPadding(m,2) + 'm ' + zeroPadding(s,2) + 's'
 }
 
-function printWait(diff) {
+function printGroup(diff, groupName, nextGroup) {
     // 要素取得
     let intitle = document.getElementById('intitle');
     let viewarea = document.getElementById('lifepoint');
     let subviewarea = document.getElementById('sublifepoint');
-    intitle.innerHTML = 'YOU CAN DO IT !!';
+    intitle.innerHTML = groupName;
     // 単位変換
     let sec = parseInt(diff/1000);
     let h = sec%86400/3600 | 0;
     let m = sec%86400%3600/60 | 0;
     let s = sec%60;
-    viewarea.innerHTML = zeroPadding(h,2) + 'h ' + zeroPadding(m,2) + 'm ' + zeroPadding(s,2) + 's'
-    subviewarea.innerHTML = 'Now is publishing\nthe graduation thesis';
+    viewarea.innerHTML = zeroPadding(m,2) + 'm ' + zeroPadding(s,2) + 's'
+    subviewarea.innerHTML = 'Next group is ' + nextGroup;
 }
 
 function printAfter(diff) {
@@ -57,9 +57,15 @@ const view = function() {
     let now = ((new Date()).getTime()) + (new Date()).getTimezoneOffset()*60*1000;
 
     // 第一目標
-    let endpoint1 = (new Date(2020, 0, 31, 16, 45, 0, 0)).getTime() + (new Date()).getTimezoneOffset()*60*1000;
-    let endpoint2 = (new Date(2020, 1, 13, 14, 50, 0, 0)).getTime() + (new Date()).getTimezoneOffset()*60*1000;
-    let endpoint3 = (new Date(2020, 1, 13, 16, 30, 0, 0)).getTime() + (new Date()).getTimezoneOffset()*60*1000;
+    let endpoint1 = (new Date(2020, 0, 31, 16, 45, 0, 0)).getTime() + (new Date()).getTimezoneOffset()*60*1000; // 提出
+    let endpoint2 = (new Date(2020, 1, 13, 14, 50, 0, 0)).getTime() + (new Date()).getTimezoneOffset()*60*1000; // マイコン
+    let endpoint3 = (new Date(2020, 1, 13, 15, 5, 0, 0)).getTime() + (new Date()).getTimezoneOffset()*60*1000; // 休憩
+    let endpoint4 = (new Date(2020, 1, 13, 15, 20, 0, 0)).getTime() + (new Date()).getTimezoneOffset()*60*1000; // サッカー
+    let endpoint5 = (new Date(2020, 1, 13, 15, 35, 0, 0)).getTime() + (new Date()).getTimezoneOffset()*60*1000; // 人狼
+    let endpoint6 = (new Date(2020, 1, 13, 15, 50, 0, 0)).getTime() + (new Date()).getTimezoneOffset()*60*1000; // viewer
+    let endpoint7 = (new Date(2020, 1, 13, 16, 00, 0, 0)).getTime() + (new Date()).getTimezoneOffset()*60*1000; // dcop
+    let endpoint8 = (new Date(2020, 1, 13, 16, 15, 0, 0)).getTime() + (new Date()).getTimezoneOffset()*60*1000; // map
+    let endpoint9 = (new Date(2020, 1, 13, 16, 25, 0, 0)).getTime() + (new Date()).getTimezoneOffset()*60*1000; // after
 
     if (now < endpoint1) {
         printTimer(endpoint1 - now);
@@ -68,9 +74,28 @@ const view = function() {
         printTimer(endpoint2 - now);
     }
     else if (now < endpoint3) {
-        printWait(endpoint3 - now);
-    } else {
-        printAfter(now - endpoint3);
+        printGroup(endpoint3 - now, 'Micro Computer', 'RSS');
+    }
+    else if (now < endpoint4) {
+        printGroup(endpoint4 - now, 'Break Time', 'RSS');
+    }
+    else if (now < endpoint5) {
+        printGroup(endpoint5 - now, 'RSS', 'AIWolf');
+    }
+    else if (now < endpoint6) {
+        printGroup(endpoint6 - now, 'AIWolf', 'Viewer');
+    }
+    else if (now < endpoint7) {
+        printGroup(endpoint7 - now, 'Viewer', 'DCOP');
+    }
+    else if (now < endpoint8) {
+        printGroup(endpoint8 - now, 'DCOP', 'MAP');
+    }
+    else if (now < endpoint9) {
+        printGroup(endpoint9 - now, 'MAP', 'NONE');
+    }
+    else {
+        printAfter(now - endpoint9);
     }
 }
 
